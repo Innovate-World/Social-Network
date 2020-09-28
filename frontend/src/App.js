e4b1bc3d9d5c0fd2244f45db4291f273335f9f27
saipavan4518 <mvspavan001@gmail.com>
2020-09-28 08:30:52 +0530

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>hello this is sample website</p>
+      <p>hello this is another website</p>
+    </div>
+  );
+}
+
+export default App;
